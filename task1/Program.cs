b0d7e4595228ce46336c84e6678d8f00b1036020
Timur <xanov.timur@mail.ru>
2023-06-23 07:49:18 +0300

    Home Work3

---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом 
+Console.WriteLine("Введите пятизначное число: "); 
+int N = int.Parse(Console.ReadLine()); 
+ 
+int result = 0; 
+int N1 = N; 
+ 
+if (N > 9999 && N < 100000) 
+{ 
+    while (N1 != 0) 
+    { 
+        result = (result * 10) + (N1 % 10); 
+        N1 = N1 / 10; 
+    } 
+    if (N == result) 
+    { 
+    Console.WriteLine("Число является палиндромом"); 
+    } 
+    else 
+    { 
+        Console.WriteLine("Число не является палиндромом"); 
+    } 
+} 
+else 
+{ 
+    Console.WriteLine("Число не пятизначное"); 
+}
---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве. 
+ 
+ 
+Console.WriteLine("Введите x1"); 
+double x1 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите y1"); 
+double y1 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите z1"); 
+double z1 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите x2"); 
+double x2 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите y2"); 
+double y2 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите z2"); 
+double z2 = int.Parse(Console.ReadLine()); 
+ 
+double A = x2 - x1; 
+double B = y2 - y1; 
+double C = z1 - z2; 
+ 
+double length = Math.Sqrt(A * A + B * B + C * C); 
+Console.WriteLine($"Длинна отрезка {length}");
